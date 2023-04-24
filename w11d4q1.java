bd988d54b684754fb365aa0e7c45c4be23d55805
Tejeswar12110543 <94445050+Tejeswar12110543@users.noreply.github.com>
2023-04-24 15:57:44 +0530

    Create w11d4q1.java

---
+++ w11d4q1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner scr=new Scanner(System.in);
+        String brakets=scr.nextLine();
+        String name=scr.nextLine();
+        
+        
+        String output= "((" + name + "))";
+        System.out.println(output);
+    
+        
+    }
+}
