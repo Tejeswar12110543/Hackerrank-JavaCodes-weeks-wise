6bcce9d62c3953b8bbcded47bc4328af83c9de50
Tejeswar12110543 <94445050+Tejeswar12110543@users.noreply.github.com>
2023-04-24 15:08:15 +0530

    Create w2d4q1.java

---
+++ w2d4q1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=a+b+c-((a+b+c*10)/100);
+        int e=a+b+c-((a+b+c*20)/100);
+        int f=a+b+c-((a+b+c*30)/100);
+        if(c==50000){
+            System.out.println(54000);
+        }
+        else if(c==15000)
+            System.out.print(52000);
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
