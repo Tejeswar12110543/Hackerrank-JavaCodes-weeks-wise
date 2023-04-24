1f73c2a00a679c9c6daa610ae4f5e03a4ae8a6b9
Tejeswar12110543 <94445050+Tejeswar12110543@users.noreply.github.com>
2023-04-24 15:04:42 +0530

    Create w2d2q1.java

---
+++ w2d2q1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner ob=new Scanner(System.in);
+        double a=ob.nextDouble();
+        if(a!=0 && a<5)
+            System.out.print("Thanks");
+        else
+        System.out.print("Error: 101");       
+    }
+}
