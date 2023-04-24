5997cdd38c089ddab8408308990d251973dc4e6f
Tejeswar12110543 <94445050+Tejeswar12110543@users.noreply.github.com>
2023-04-24 15:11:13 +0530

    Create w3d1q1.java

---
+++ w3d1q1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char c = sc.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
