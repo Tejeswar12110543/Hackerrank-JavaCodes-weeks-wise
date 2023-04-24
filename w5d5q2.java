18878125fc450f21e76eea7e7bca5ca1ad6a5505
Tejeswar12110543 <94445050+Tejeswar12110543@users.noreply.github.com>
2023-04-24 15:36:24 +0530

    Create w5d5q2.java

---
+++ w5d5q2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+
+        int a=scr.nextInt();
+
+        int b=scr.nextInt();
+
+        if(a>b)
+
+        {
+
+            System.out.print("true");
+
+        }
+
+        else
+
+        {
+
+            System.out.print("false");
+
+        }
+
+    }
+
+}
