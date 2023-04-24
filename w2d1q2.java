c286412a9d4db5fcb86b33e4475ff84ec44dea30
Tejeswar12110543 <94445050+Tejeswar12110543@users.noreply.github.com>
2023-04-24 15:00:36 +0530

    Create w2d1q2.java

---
+++ w2d1q2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int X=sc.nextInt();
+        int Y=N+X;
+        System.out.println(+Y);
+    }
+}
